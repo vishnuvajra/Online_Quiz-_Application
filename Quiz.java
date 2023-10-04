612112e9ae81699c82ca36455835e4b1822722eb
vishnuvajra <96058813+vishnuvajra@users.noreply.github.com>
2023-10-04 20:24:22 +0530

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -27,7 +27,7 @@ public class Quiz extends JFrame implements ActionListener {
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
         
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/quiz.jpg"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("quiz.jpg"));
         JLabel image = new JLabel(i1);
         image.setBounds(0, 0, 1440, 392);
         add(image);
